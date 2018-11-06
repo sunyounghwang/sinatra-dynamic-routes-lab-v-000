1febc9eed1e5833e781da71915c3c8cc53dc138b
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-11-06 22:51:06 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -18,7 +18,6 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
-    binding.pry
     "#{params[:number1].to_i + params[:number2].to_i}" if params[:operation] == "add"
     "#{params[:number1].to_i - params[:number2].to_i}" if params[:operation] == "subtract"
     "#{params[:number1].to_i / params[:number2].to_i}" if params[:operation] == "divide"
