6bd0fc416f67b81a0023b4c2aed417079574f236
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-11-06 22:53:18 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -21,11 +21,12 @@ class App < Sinatra::Base
     case params[:operation]
     when "add"
       "#{params[:number1].to_i + params[:number2].to_i}"
-    when "subtract"     
+    when "subtract"
       "#{params[:number1].to_i - params[:number2].to_i}"
     when "divide"
       "#{params[:number1].to_i / params[:number2].to_i}"
     when "multiply"
       "#{params[:number1].to_i * params[:number2].to_i}"
+    end
   end
 end
