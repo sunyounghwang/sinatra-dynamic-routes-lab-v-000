d634ca070bfdc304f9adc091f8278e2564e7be15
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-11-06 22:49:53 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -18,9 +18,9 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
-    "#{params[:number1].to_i + params[:number2]}" if params[:operation] == "add"
-    "#{params[:number1].to_i - params[:number2]}" if params[:operation] == "subtract"
-    "#{params[:number1].to_i / params[:number2]}" if params[:operation] == "divide"
-    "#{params[:number1].to_i * params[:number2]}" if params[:operation] == "multiply"
+    "#{params[:number1].to_i + params[:number2].to_i}" if params[:operation] == "add"
+    "#{params[:number1].to_i - params[:number2].to_i}" if params[:operation] == "subtract"
+    "#{params[:number1].to_i / params[:number2].to_i}" if params[:operation] == "divide"
+    "#{params[:number1].to_i * params[:number2].to_i}" if params[:operation] == "multiply"
   end
 end
